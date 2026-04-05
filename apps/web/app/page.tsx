5541c805b5161dd8aaef9905d32c11bf5f3c35ea
o1key222k <o1key222k@gmail.com>
2026-04-06 00:09:30 +0700

    feat: initialize monorepo structure with Tailwind CSS v4, shared UI package, and landing page application

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { Button } from "@repo/ui";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
@@ -48,7 +48,7 @@ export default function Home() {
 
         <div className={styles.ctas}>
           <a
-            className={styles.primary}
+            className={[styles.cta, styles.primary].join(" ")}
             href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
             target="_blank"
             rel="noopener noreferrer"
@@ -66,12 +66,12 @@ export default function Home() {
             href="https://turborepo.dev/docs?utm_source"
             target="_blank"
             rel="noopener noreferrer"
-            className={styles.secondary}
+            className={[styles.cta, styles.secondary].join(" ")}
           >
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
+        <Button className={[styles.cta, styles.secondary].join(" ")}>
           Open alert
         </Button>
       </main>
