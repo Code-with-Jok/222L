9fea3079677d243da65cedee82cb5d5ba96fde8e
o1key222k <o1key222k@gmail.com>
2026-04-14 20:30:04 +0700

    feat: initialize modular monolith architecture with Prisma data models and project documentation

--- apps/landing-page/app/page.tsx
+++ apps/landing-page/app/page.tsx
@@ -1,19 +1,13 @@
 import HeroBackground from "@/components/HeroBackground";
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
-import { prisma } from "@repo/db";
 
-// export default function Home() {
-//   return (
-//     <div className="relative min-h-screen w-full overflow-x-hidden">
-//       <HeroBackground />
-//       <Header />
-//       <HeroSection />
-//     </div>
-//   );
-// }
-
-export default async function Home() {
-  const user = await prisma.user.findFirst();
-  return <div>{user?.name ?? "No user added yet"}</div>;
+export default function Home() {
+  return (
+    <div className="relative min-h-screen w-full overflow-x-hidden">
+      <HeroBackground />
+      <Header />
+      <HeroSection />
+    </div>
+  );
 }
--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/client/index.js";
+import { Pool } from "pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
